7a72458bb6c454cbb9f162f215754a7b4a82b570
nikhil-mangla <e22cseu0288@bennett.edu.in>
2025-07-20 01:08:11 +0530

    fixed db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,13 +4,16 @@ import { db } from "@/db"; // your drizzle instance
 import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true, 
-      }, 
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-        schema:{
-            ...schema, 
-        },
-    }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      user: schema.user, // <-- this must match your export!
+      account: schema.account,
+      session: schema.session,
+      verification: schema.verification,
+    },
+  }),
 });
