df02b124537860334db436b0a7d20028594c15d5
nikhil-mangla <e22cseu0288@bennett.edu.in>
2025-07-24 14:08:09 +0530

    05-authentication-socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { HomeView} from "@/modules/home/ui/views/home-view"
+import { auth } from '@/lib/auth'
+import { headers } from "next/headers";
+import { redirect } from "next/navigation"
 
 
-export default function Home() {
-  return (
-    <div>
-  hello world!
- </div>
-  )
- 
+const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    if(!session){
+    redirect("/sign-in")
 }
+
+
+return <HomeView />
+};
+export default Page;
+
